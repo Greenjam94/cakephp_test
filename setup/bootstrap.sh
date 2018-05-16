80c885d3efdcab6002955bc4d3e3ba27701641da
James Green <5766832+Greenjam94@users.noreply.github.com>
2018-05-15 23:37:58 -0400

    CakePHP installation

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -13,13 +13,13 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 
 apt-get -y install mysql-server
 mysql -u root -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
-mysql -u lcori -p$DBPASSWD < /vagrant/setup/db.sql
+mysql -u dbuser -p$DBPASSWD < /vagrant/setup/db.sql
 
 cd /vagrant
 sudo curl -s https://getcomposer.org/installer | php
 
 rm -rf /var/www/html
-sudo ln -fs /vagrant/webapp /var/www/html
+sudo ln -fs /vagrant/cms /var/www/html
 
 sudo cp /vagrant/setup/apache.conf /etc/apache2/sites-enabled/000-default.conf
 sudo a2enmod rewrite
